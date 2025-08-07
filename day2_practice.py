bef135cdc336bf1059a78df9c49272d8f4806f74
LaMagicienne <ekonomerveille@gmail.com>
2025-08-07 11:56:45 +0100

    Updated Day 2 practice file: printing variables

--- day2_practice.py
+++ day2_practice.py
@@ -1,1 +1,10 @@
 "# Day 2 Practice" 
+print("Welcome to Data Science!")
+print ("Let's explore Python syntaxe together")
+
+my_name = "Merveille"
+my_age = 23
+fav_number = 15
+
+print(f"My name is {my_name}, I am {my_age} years old, and my favorite number is {fav_number}.")
+
